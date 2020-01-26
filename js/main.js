32c1658b432f5bc2a0a2a446f51af80822ec274c
Jorge A. F. Núñez <jorgeadrianflores@hotmail.com>
2020-01-25 19:00:34 -0600

    update files

--- js/main.js
+++ js/main.js
@@ -171,3 +171,20 @@ app.get('/faq/', (req, res) => {
 app.get('/code-of-conduct/', (req, res) => {
   res.sendFile('./code-of-conduct.html', { root: __dirname });
 });
+
+// Speakers-Pointers
+app.get('/speakers/benjamin/', (req, res) => {
+  res.sendFile('./speakers-benjamin.html', { root: __dirname });
+});
+
+app.get('/speakers/alejandro/', (req, res) => {
+  res.sendFile('./speakers-alejandro.html', { root: __dirname });
+});
+
+app.get('/speakers/gustavo/', (req, res) => {
+  res.sendFile('./speakers-gustavo.html', { root: __dirname });
+});
+
+app.get('/speakers/andres/', (req, res) => {
+  res.sendFile('./speakers-andres.html', { root: __dirname });
+});
